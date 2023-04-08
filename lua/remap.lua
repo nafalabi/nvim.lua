d0d3829c032e83b7a8fc19915e9b6542ed9f4d11
Nanda Abi Fahmi <nafal.abi@gmail.com>
2023-04-08 13:25:58 +0700

    update remap switch buffer

--- lua/remap.lua
+++ lua/remap.lua
@@ -35,3 +35,6 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+vim.keymap.set("n", "<C-[>", "<cmd>bprev<CR>");
+vim.keymap.set("n", "<C-]>", "<cmd>bnext<CR>");
+
