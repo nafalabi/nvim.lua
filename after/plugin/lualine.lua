27e108ad1ce107652427c89e0759ff31da14c1dc
Nanda Abi Fahmi <nafal.abi@gmail.com>
2023-04-08 13:26:23 +0700

    update full path filename lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,10 +1,18 @@
 -- Set lualine as statusline
 -- See `:help lualine.txt`
 require('lualine').setup {
-  options = {
-    icons_enabled = false,
-    component_separators = '|',
-    section_separators = '',
-  },
+    options = {
+        icons_enabled = false,
+        component_separators = '|',
+        section_separators = '',
+    },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            }
+        }
+    }
 }
 
