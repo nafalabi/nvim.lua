b57d213450a91415eb964fcf0e9c820d58630ea7
Nanda Abi Fahmi <nafal.abi@gmail.com>
2023-04-16 12:29:44 +0700

    update remamp

--- lua/remap.lua
+++ lua/remap.lua
@@ -35,6 +35,3 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<C-[>", "<cmd>bprev<CR>");
-vim.keymap.set("n", "<C-]>", "<cmd>bnext<CR>");
-
