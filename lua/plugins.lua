4b3c414d89b473c0726f4c1106454cdd6219a153
Nanda Abi Fahmi <nafal.abi@gmail.com>
2024-06-14 14:45:06 +0700

    use alternative minimap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -114,28 +114,59 @@ require("lazy").setup({
 			})
 		end,
 	},
+	-- {
+	-- 	"wfxr/minimap.vim",
+	-- 	init = function()
+	-- 		vim.cmd("let g:minimap_width = 10")
+	-- 		vim.cmd("let g:minimap_auto_start = 0")
+	-- 		vim.cmd("let g:minimap_auto_start_win_enter = 1")
+	-- 		vim.cmd("let g:minimap_git_colors	= 1")
+	-- 		vim.cmd("let g:minimap_highlight_search = 1")
+	--
+	-- 		-- TODO: this might need some improvement later
+	-- 		vim.api.nvim_create_autocmd({ "BufLeave" }, {
+	-- 			callback = function(event)
+	-- 				if event.event == "BufLeave" then
+	-- 					local nvimtree = "NvimTree"
+	-- 					if string.match(event.file, nvimtree) == nvimtree then
+	-- 						vim.defer_fn(function()
+	-- 							vim.cmd("Minimap")
+	-- 						end, 500)
+	-- 					end
+	-- 				end
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
 	{
-		"wfxr/minimap.vim",
-		init = function()
-			vim.cmd("let g:minimap_width = 10")
-			vim.cmd("let g:minimap_auto_start = 0")
-			vim.cmd("let g:minimap_auto_start_win_enter = 1")
-      vim.cmd("let g:minimap_git_colors	= 1")
-      vim.cmd("let g:minimap_highlight_search = 1")
-
-			-- TODO: this might need some improvement later
-			vim.api.nvim_create_autocmd({ "BufLeave" }, {
-				callback = function(event)
-					if event.event == "BufLeave" then
-						local nvimtree = "NvimTree"
-						if string.match(event.file, nvimtree) == nvimtree then
-							vim.defer_fn(function()
-								vim.cmd("Minimap")
-							end, 500)
-						end
-					end
-				end,
+		"gorbit99/codewindow.nvim",
+		config = function()
+			local codewindow = require("codewindow")
+			codewindow.setup({
+				auto_enable = true,
+				minimap_width = 15,
+				screen_bounds = "background",
 			})
+			codewindow.apply_default_keybinds()
+
+			-- codewindow.setup({
+			--   active_in_terminals = false, -- Should the minimap activate for terminal buffers
+			--   auto_enable = false, -- Automatically open the minimap when entering a (non-excluded) buffer (accepts a table of filetypes)
+			--   exclude_filetypes = { 'help' }, -- Choose certain filetypes to not show minimap on
+			--   max_minimap_height = nil, -- The maximum height the minimap can take (including borders)
+			--   max_lines = nil, -- If auto_enable is true, don't open the minimap for buffers which have more than this many lines.
+			--   minimap_width = 20, -- The width of the text part of the minimap
+			--   use_lsp = true, -- Use the builtin LSP to show errors and warnings
+			--   use_treesitter = true, -- Use nvim-treesitter to highlight the code
+			--   use_git = true, -- Show small dots to indicate git additions and deletions
+			--   width_multiplier = 4, -- How many characters one dot represents
+			--   z_index = 1, -- The z-index the floating window will be on
+			--   show_cursor = true, -- Show the cursor position in the minimap
+			--   screen_bounds = 'lines' -- How the visible area is displayed, "lines": lines above and below, "background": background color
+			--   window_border = 'single' -- The border style of the floating window (accepts all usual options)
+			--   relative = 'win' -- What will be the minimap be placed relative to, "win": the current window, "editor": the entire editor
+			--   events = { 'TextChanged', 'InsertLeave', 'DiagnosticChanged', 'FileWritePost' } -- Events that update the code window
+			-- })
 		end,
 	},
 
